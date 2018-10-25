3c20f027adedbd4956a628333ee2bfeede6cd54c
Victor <vb4007@gmail.com>
2018-10-25 22:45:13 +0100

    Support for JSP views

---
+++ src/main/java/euteam/cleanmachine/views/WelcomeController.java
@@ -0,0 +1,17 @@
+package euteam.cleanmachine.views;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8100" )
+public class WelcomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView list(ModelAndView mav) {
+
+        mav.setViewName("welcome");
+        mav.addObject("message", "Hello JSP!");
+
+        return mav;
+    }
+}

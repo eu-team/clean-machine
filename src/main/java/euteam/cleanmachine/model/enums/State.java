dbe2be9d39d52ae7ed8a2ea473a3a93bdcdda12b
Vincent R <31095208+AmosTrask@users.noreply.github.com>
2018-11-17 23:05:30 +0000

    Adding reserved state

--- src/main/java/euteam/cleanmachine/model/enums/State.java
+++ src/main/java/euteam/cleanmachine/model/enums/State.java
@@ -1,7 +1,8 @@
 package euteam.cleanmachine.model.enums;
 
 public enum State {
-    IDLE("Idle"), AUTHENTICATED("Authenticated"), OUTOFORDER("Out of order"), RUNNING("Running"), LOCKED("Locked");
+
+    IDLE("Idle"), AUTHENTICATED("Authenticated"), OUTOFORDER("Out of order"), RUNNING("Running"),  LOCKED("Locked");
 
     private String name;
 

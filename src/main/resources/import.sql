41eec2b7b9a72896a949983f5baff28354ce3cf5
Vincent R <31095208+AmosTrask@users.noreply.github.com>
2018-11-17 12:10:26 +0000

    Create an administrator in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,6 +3,7 @@ INSERT INTO role (ID, role_name) VALUES (2, 'ROLE_MAINTAINER');
 INSERT INTO role (ID, role_name) VALUES (3, 'ROLE_CUSTOMER');
 
 INSERT INTO public.users VALUES ('Customer', 100, 'email@domain.com', 'Name', '$2a$10$4g7r7UA3UjlU08A4NabsG.Jt3rYTRWlHZDk3KcjPB.HybTML3aK4a', 'username', 3, NULL);
+INSERT INTO public.users VALUES ('Administrator', 200, 'admin@domain.com', 'Admin', '$2a$10$4g7r7UA3UjlU08A4NabsG.Jt3rYTRWlHZDk3KcjPB.HybTML3aK4a', 'admin', 1, NULL);
 INSERT INTO "public"."account" VALUES (100, 0, 100);
 INSERT INTO auth_item VALUES ('NFCCard', 100, 1234567890);
 INSERT INTO users_auth_item_list VALUES (100, 100);

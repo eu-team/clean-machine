f59cab7162f1266066aadccf12035e415883ac10
Vincent R <31095208+AmosTrask@users.noreply.github.com>
2018-11-17 23:04:59 +0000

    adding maintainer in import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,6 +6,7 @@ INSERT INTO public.facility VALUES (100, '8, facility avenue, Limerick', 'Lavoma
 
 INSERT INTO public.users VALUES ('Customer', 100, 'email@domain.com', 'Name', '$2a$10$4g7r7UA3UjlU08A4NabsG.Jt3rYTRWlHZDk3KcjPB.HybTML3aK4a', 'username', 3, NULL);
 INSERT INTO public.users VALUES ('Administrator', 200, 'admin@domain.com', 'Admin', '$2a$10$4g7r7UA3UjlU08A4NabsG.Jt3rYTRWlHZDk3KcjPB.HybTML3aK4a', 'admin', 1, 100);
+INSERT INTO public.users VALUES ('Maintainer', 300, 'maintainer@domain.com', 'Maintainer', '$2a$10$4g7r7UA3UjlU08A4NabsG.Jt3rYTRWlHZDk3KcjPB.HybTML3aK4a', 'maintainer', 2, 100);
 INSERT INTO "public"."account" VALUES (100, 0, 100);
 INSERT INTO auth_item VALUES ('NFCCard', 100, 1234567890);
 INSERT INTO users_auth_item_list VALUES (100, 100);

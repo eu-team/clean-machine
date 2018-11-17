911f9453dbe81f5117d95921bb5832a8b2be406d
Victor <vb4007@gmail.com>
2018-11-17 14:08:50 +0000

    Infer account from authentication context

--- src/main/java/euteam/cleanmachine/controller/AccountController.java
+++ src/main/java/euteam/cleanmachine/controller/AccountController.java
@@ -4,6 +4,7 @@ import euteam.cleanmachine.dto.AccountSubscriptionDto;
 import euteam.cleanmachine.dto.SubscribeToPlanDto;
 import euteam.cleanmachine.exceptions.ServiceException;
 import euteam.cleanmachine.model.user.Account;
+import euteam.cleanmachine.model.user.Customer;
 import euteam.cleanmachine.model.user.User;
 import euteam.cleanmachine.security.JwtTokenUtil;
 import euteam.cleanmachine.service.AccountService;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -57,8 +60,14 @@ public class AccountController {
     @RequestMapping(path="/account/subscribe", method = RequestMethod.POST)
     public ResponseEntity<?> subscribeToPlan(@RequestBody @Valid SubscribeToPlanDto subscribeToPlanDto) {
         try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            String username = auth.getName();
+            User user = userService.getUserByUsername(username);
+
+            Account account = accountService.getAccountByUser(user);
+
             AccountSubscriptionDto accountSubscriptionDto
-                    = accountService.subscribeToPlan(subscribeToPlanDto.getAccountID(), subscribeToPlanDto.getSubscriptionPlanID());
+                    = accountService.subscribeToPlan(account.getId(), subscribeToPlanDto.getSubscriptionPlanID());
 
             return ResponseEntity.status(201).body(accountSubscriptionDto);
         } catch (ServiceException e) {
